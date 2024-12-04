8833662c0ec85887e57d17719c25a8e0ec8a384b
Rishi Varman Rathimala Thangaravi <rs329673@dal.ca>
2024-12-04 02:31:55 -0400

    lambda changes

--- backend/utils/lamdaSecrets.js
+++ backend/utils/lamdaSecrets.js
@@ -8,20 +8,13 @@ const GCP_SECRET_URL = process.env.LAMBDA_GCP_SECRET_URL;
 const fetchAWSSecret = async () => {
   try {
     const response = await axios.get(AWS_SECRET_URL); 
-    const { statusCode, body } = response.data; 
+    const {secret } = response.data; 
 
-    if (statusCode !== 200) {
-      throw new Error(`Failed to fetch AWS secrets. Status code: ${statusCode}`);
-    }
-
-    const parsedBody = JSON.parse(body); // Parse the body as JSON
-    const secrets = parsedBody.secret; // Extract the "secret" object
-
-    if (!secrets) {
+    if (!secret) {
       throw new Error("Secrets not found in the response.");
     }
 
-    return secrets;
+    return secret;
   } catch (error) {
     console.error("Error fetching AWS secrets:", error.message);
     throw new Error("Unable to retrieve AWS secrets.");
@@ -32,20 +25,13 @@ const fetchAWSSecret = async () => {
 const fetchGCPSecret = async () => {
     try {
       const response = await axios.get(GCP_SECRET_URL); 
-      const { statusCode, body } = response.data; 
-  
-      if (statusCode !== 200) {
-        throw new Error(`Failed to fetch GCP secrets. Status code: ${statusCode}`);
-      }
-  
-      const parsedBody = JSON.parse(body); // Parse the body as JSON
-      const secrets = parsedBody.secret; // Extract the "secret" object
-  
-      if (!secrets) {
-        throw new Error("Secrets not found in the response.");
-      }
-  
-      return secrets;
+      const {secret } = response.data; 
+
+    if (!secret) {
+      throw new Error("Secrets not found in the response.");
+    }
+
+    return secret;
     } catch (error) {
       console.error("Error fetching GCP secrets:", error.message);
       throw new Error("Unable to retrieve GCP secrets.");
---
+++ frontend/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    devServer: {
+      hot: false, // Disable Hot Module Replacement
+      liveReload: false, // Disable Live Reload
+    },
+  };
+  
